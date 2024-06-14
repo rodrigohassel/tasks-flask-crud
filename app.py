2c25f7ffa0e21b666bbd427e26b233d45165138c
Rodrigo Hassel <rodri.hassel@gmail.com>
2024-06-14 08:58:49 -0300

    Add new routes for task models API as: create, get all and get specific

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
-
+from flask import Flask, request, jsonify
+from models.task import Task
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "Hello world"
+tasks = []
+task_id_control = 1
+
+@app.route('/tasks', methods=['POST'])
+def create_task():
+    global task_id_control
+
+    data = request.get_json()
+
+    new_task = Task(
+        title=data['title'], 
+        description=data.get("description", ""), 
+        id=task_id_control)
+    
+    task_id_control += 1
+
+    tasks.append(new_task)
+
+    return jsonify({"message": "Nova tarefa criada com sucesso"})
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    task_list = [ task.to_dict() for task in tasks ]
+    
+    output = {
+        "tasks": task_list,
+        "total_tasks": len(task_list)
+    }
+    
+    return jsonify(output) 
+
+@app.route('/tasks/<int:id>', methods=['GET'])
+def get_task(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.to_dict())
+            
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
