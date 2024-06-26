407362e2ef3b7fbe01de6626b7cdb3e4981ac507
Rodrigo Hassel <rodri.hassel@gmail.com>
2024-06-26 20:51:45 -0300

    add tests via pytest lib
    - test route tasks (CRUD)

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def create_task():
 
     tasks.append(new_task)
 
-    return jsonify({"message": "Nova tarefa criada com sucesso"})
+    return jsonify({"message": "Nova tarefa criada com sucesso", "id": new_task.id})
 
 @app.route('/tasks', methods=['GET'])
 def get_tasks():
